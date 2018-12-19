ea5dea8ace6ea6af3f134c539ea7d3d5067389b0
Juanjo Molinero <jjmolinero@gmail.com>
2018-12-19 15:53:21 +0100

    Add failing acceptance test for the movie list
    We create a test for the use case that a user goes to the movies URL and should
    be able to see a list of movies.

---
+++ acceptance-tests/movie_list_tests.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import unittest
+
+
+class MovieListTests(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_should_contain_a_list_of_movies(self):
+        # if the user goes to the main app url
+        self.browser.get('http://localhost:8000/movies')
+
+        # she should be able to see a plain list of all movies Studio Ghibli
+        # movies
+        movies = self.browser.find_elements_by_class_name('movie')
+        self.assertGreater(len(movies), 10)
+
+        # each one with its own title, description and release date
+        for movie in movies:
+            title = movie.find_elements_by_class_name('title')
+            self.assertTrue(title.strip())
+
+            description = movie.find_elements_by_class_name('description')
+            self.assertTrue(description.strip())
+
+            release_date = movie.find_elements_by_class_name('release-date')
+            self.assertTrue(release_date.strip().isdigit())
+
+
+if __name__ == '__main__':
+    unittest.main()
