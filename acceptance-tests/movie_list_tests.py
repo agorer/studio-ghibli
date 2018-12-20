a2b331a11c54361b7dc1eea4b4cb7c0cfe415e8d
Juanjo Molinero <jjmolinero@gmail.com>
2018-12-20 09:22:34 +0100

    Show movie list info on the template
    Tested by the acceptance test previously created. With this step
    we have finished with the first use case of the application (showing
    the list of movies).

--- acceptance-tests/movie_list_tests.py
+++ acceptance-tests/movie_list_tests.py
@@ -20,14 +20,14 @@ class MovieListTests(unittest.TestCase):
 
         # each one with its own title, description and release date
         for movie in movies:
-            title = movie.find_elements_by_class_name('title')
-            self.assertTrue(title.strip())
+            title = movie.find_element_by_class_name('title')
+            self.assertTrue(title.text.strip())
 
-            description = movie.find_elements_by_class_name('description')
-            self.assertTrue(description.strip())
+            description = movie.find_element_by_class_name('description')
+            self.assertTrue(description.text.strip())
 
-            release_date = movie.find_elements_by_class_name('release-date')
-            self.assertTrue(release_date.strip().isdigit())
+            release_date = movie.find_element_by_class_name('release-date')
+            self.assertTrue(release_date.text.strip().isdigit())
 
 
 if __name__ == '__main__':
